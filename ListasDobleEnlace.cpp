4cb602566713b8b9216073c8f591705050b82b37
JuanNeftaly <104848289+JuanNeftaly@users.noreply.github.com>
2022-10-18 21:39:57 -0600

    funtional code :white_check_mark:

--- ListasDobleEnlace.cpp
+++ ListasDobleEnlace.cpp
@@ -27,38 +27,34 @@ int main()
 
     do
     {
-        cout << "Cuantos datos desea ingresar. " << endl;
-        cout << "Respuesta: ";
-        cin >> NumDatos;
         int Opcion;
-        cout << "Que desea hacer? " << endl;
-        cout << "1. Insertar datos al Inicio" << endl;
-        cout << "2. Insertar datos al final" << endl;
-        cout << "3. Eliminar datos del Inicio" << endl;
-        cout << "4. Eliminar datos del Final" << endl;
-        cout << "5. Imprimir " << endl;
-        cout << "6. Salir " << endl;
+        cout << endl;
+        cout << "Bienvenido. Que desea hacer? " << endl;
+        cout << "::::::::::::::::::::::::::::::::::" << endl;
+        cout << ":: 1. Insertar datos al Inicio  ::" << endl;
+        cout << ":: 2. Insertar datos al final   ::" << endl;
+        cout << ":: 3. Eliminar datos del Inicio ::" << endl;
+        cout << ":: 4. Eliminar datos del Final  ::" << endl;
+        cout << ":: 5. Imprimir                  ::" << endl;
+        cout << ":: 6. Salir                     ::" << endl;
+        cout << "::::::::::::::::::::::::::::::::::" << endl;
+        cout << endl;
+        cout << "Option: ";
         cin >> Opcion;
 
         switch (Opcion)
         {
         case 1:
-            for (int i = 0; i < NumDatos; i++)
-            {
-                cout << "Ingrese el dato: ";
-                cin >> Dato;
-                InsertarInicio(Dato);
-            }
+            cout << "Ingrese el dato: ";
+            cin >> Dato;
+            InsertarInicio(Dato);
 
             break;
 
         case 2:
-            for (int i = 0; i < NumDatos; i++)
-            {
-                cout << "Ingrese el dato: ";
-                cin >> Dato;
-                InsertarFinal(Dato);
-            }
+            cout << "Ingrese el dato: ";
+            cin >> Dato;
+            InsertarFinal(Dato);
 
             break;
 
@@ -106,6 +102,8 @@ void InsertarInicio(int n)
         if (lista == NULL)
         {
             lista = nuevoNodo;
+
+            cout << "Elemento agregado al inicio " << endl;
         }
         else
         {
@@ -113,11 +111,13 @@ void InsertarInicio(int n)
             nuevoNodo->siguiente = lista;
             lista->anterior = nuevoNodo;
             lista = nuevoNodo;
+
+            cout << "Elemento agregado al inicio " << endl;
         }
     }
     else
     {
-        cout << "No se pueden agregar mas nodos";
+        cout << "No se pueden agregar mas nodos" << endl;
     }
 }
 
@@ -135,6 +135,8 @@ void InsertarFinal(int n)
         if (lista == NULL)
         {
             lista = nuevoNodo;
+
+            cout << "Elemento agregado al final " << endl;
         }
         else
         {
@@ -145,11 +147,13 @@ void InsertarFinal(int n)
             }
             temporal->siguiente = nuevoNodo;
             nuevoNodo->anterior = temporal;
+
+            cout << "Elemento agregado al final " << endl;
         }
     }
     else
     {
-        cout << "No se pueden agregar mas nodos";
+        cout << "No se pueden agregar mas nodos" << endl;
     }
 }
 
@@ -171,16 +175,20 @@ void EliminarFinal()
             temporal2 = temporal->anterior;
             temporal2->siguiente = NULL;
             delete temporal;
+
+            cout << "Elemento eliminado del final " << endl;
         }
         else
         {
             delete lista;
             lista = NULL;
+
+            cout << "Elemento eliminado de final " << endl;
         }
     }
     else
     {
-        cout << "Lista vacia";
+        cout << "Lista vacia" << endl;
     }
 }
 
@@ -197,16 +205,20 @@ void EliminarInicio()
             delete temporal;
             // temporal = NULL;
             lista->anterior = NULL;
+
+            cout << "Elemento eliminado de el inicio " << endl;
         }
         else
         {
             delete lista;
             lista = NULL;
+
+            cout << "Elemento eliminado de inicio " << endl;
         }
     }
     else
     {
-        cout << "Lista vacia";
+        cout << "Lista vacia" << endl;
     }
 }
 
@@ -218,12 +230,13 @@ void Imprimir()
         // Imprimir cuando se inserta al final de la lista
         while (temporal != NULL)
         {
-            cout << "Lista " << temporal->dato << " Direccion " << temporal << " Dir siguiente " << temporal->siguiente << " Dir anterior " << temporal->anterior << endl;
+            cout << endl;
+            cout << "Lista " << temporal->dato << " Direccion " << temporal << " Direccion siguiente " << temporal->siguiente << " Direccion anterior " << temporal->anterior << endl;
             temporal = temporal->siguiente;
         }
     }
     else
     {
-        cout << "Lista vacia";
+        cout << "Lista vacia" << endl;
     }
 }
